d1d4ccca95ff69608a27d68b1b015a82846dbb3c
Samuel Wanjiku <samuel@samuelmwangi.co.ke>
2021-04-10 18:24:11 +0300

    Added payment config details

--- config/africastalking.php
+++ config/africastalking.php
@@ -4,4 +4,6 @@ return [
     'api_key' => env('AFRICASTALKING_API_KEY'),
     'username' => env('AFRICASTALKING_USERNAME'),
     'from' => env('AFRICASTALKING_FROM'),
+    'payment_product' => env('AFRICASTALKING_PAYMENT_PRODUCT', 'Sample'),
+    'currency_code' => env('AFRICASTALKING_CURRENCY_CODE', 'KES'),
 ];
